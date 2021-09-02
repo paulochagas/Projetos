faa7e78c0e1d4c04d432544e16be5abfe8fcfe2b
paulochagas <pachafi@gmail.com>
2021-09-02 12:39:30 +0100

    Initial commit

---
+++ SpringBoot/employeemanager/src/main/java/tech/getarrays/employeemanager/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package tech.getarrays.employeemanager.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
